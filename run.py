224f45bb0e02d901a5122b069288cb174841c4ab
Andy Driver <andy@pagezero.net>
2013-04-24 19:14:55 +0100

    Add werkzeug debugger

--- run.py
+++ run.py
@@ -3,3 +3,9 @@ import sys
 
 sys.path.append(os.path.join(os.path.abspath('.'), 'lib'))
 import app
+
+from werkzeug import DebuggedApplication
+
+if app.app.config['DEBUG']:
+    app.app.debug = True
+    app.app = DebuggedApplication(app.app, evalex=True)
