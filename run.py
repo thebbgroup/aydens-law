111a8c9f5db7401cc613467c78d0ffcde3f34286
Andy Driver <andy@pagezero.net>
2013-05-04 14:47:21 +0100

    Let settings determine whether to run in debug mode

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
 import app
-from werkzeug import DebuggedApplication
 
 if __name__ == '__main__':
-    app.app.debug = True
-    app.app = DebuggedApplication(app.app, evalex=True)
-    app.app.app.run('localhost', 9000)
+    app.app.run('localhost', 9000)
